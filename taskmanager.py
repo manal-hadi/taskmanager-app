cac560cb88d774ba3911c182f5bd9228cef9acfa
unknown <manalhadi.ind@gmail.com>
2025-06-08 11:56:22 +0530

    first commit

---
+++ taskmanager.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+# Task Model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    completed = db.Column(db.Boolean, default=False)
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
